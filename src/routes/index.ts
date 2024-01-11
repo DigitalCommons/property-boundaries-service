a8ed7e5b1dad5fc7c8ad5182323319c090db8d0b
rogup <rohit5.gup@gmail.com>
2024-01-11 12:14:15 +0000

    Extend route to get polygons by search area polygon
    (cherry picked from commit 36f301d4c8920e32bd3a33773e314a5554eaee8d)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,54 +7,74 @@ import {
 import {
   getPolygonsByArea,
   getPolygonsByProprietorName,
-  getPolygonsById,
+  getPolygonsByIdAndSearchArea,
 } from "../queries/query";
 
-// TODO: add error handling and appropriate responses
-async function getBoundaries(request: Request): Promise<any> {
+type GetPolygonsInBoxRequest = Request & {
+  query: {
+    sw_lng: number;
+    sw_lat: number;
+    ne_lng: number;
+    ne_lat: number;
+    secret: string;
+  };
+};
+
+// TODO:
+// - combine with search route?
+async function getPolygonsInBox(
+  request: GetPolygonsInBoxRequest,
+  h: ResponseToolkit
+): Promise<ResponseObject> {
   const { sw_lng, sw_lat, ne_lng, ne_lat, secret } = request.query;
 
   if (!secret || secret !== process.env.SECRET) {
-    return "missing or incorrect secret";
+    return h.response("missing or incorrect secret").code(403);
   }
 
-  if (!sw_lng || !sw_lat || !ne_lng || !ne_lat) return "bounds are not valid";
+  if (!sw_lng || !sw_lat || !ne_lng || !ne_lat) {
+    return h.response("bounds are not valid").code(400);
+  }
 
   const searchArea = `POLYGON ((${sw_lng} ${sw_lat}, ${ne_lng} ${sw_lat}, ${ne_lng} ${ne_lat}, ${sw_lng} ${ne_lat}, ${sw_lng} ${sw_lat}))`;
 
   const polygons = await getPolygonsByArea(searchArea);
 
   // TODO: remove this double-nested array and fix on backend API too
-  return [polygons];
+  return h.response([polygons]).code(200);
 }
 
-type GetPolygonRequest = Request & {
-  payload: { poly_ids: number[]; secret: string };
+type GetPolygonsRequest = Request & {
+  payload: {
+    poly_ids?: number[];
+    searchArea?: string;
+    secret: string;
+  };
 };
 
+/**
+ * Get polygons that:
+ * - match with the ID(s) (if given)
+ * AND
+ * - intersect with the search area (if given as a geometry in WKT format)
+ */
 async function getPolygons(
-  request: GetPolygonRequest,
-  h: ResponseToolkit,
-  d: any
+  request: GetPolygonsRequest,
+  h: ResponseToolkit
 ): Promise<ResponseObject> {
-  const { poly_ids, secret } = request.payload;
-
-  if (poly_ids.length === 0) {
-    return h.response("No poly_ids specified").code(400);
-  }
+  const { poly_ids, searchArea, secret } = request.payload;
 
   if (!secret || secret !== process.env.SECRET) {
     return h.response("missing or incorrect secret").code(403);
   }
 
-  const result = await getPolygonsById(poly_ids);
-
-  if (result.polygons.length > 0) {
-    // If at least some polygons exist, return with a 200 OK but indicate if any are missing in data
-    return h.response(result).code(200);
-  } else {
-    return h.response(result).code(404);
+  if ((!poly_ids || poly_ids.length === 0) && !searchArea) {
+    return h.response("poly_ids and/or searchArea must be given").code(400);
   }
+
+  const result = await getPolygonsByIdAndSearchArea(poly_ids, searchArea);
+
+  return h.response(result).code(200);
 }
 
 async function search(request: Request): Promise<any> {
@@ -72,17 +92,7 @@ async function search(request: Request): Promise<any> {
 const getBoundariesRoute: ServerRoute = {
   method: "GET",
   path: "/boundaries",
-  handler: getBoundaries,
-  options: {
-    auth: false,
-  },
-};
-
-// Use POST so that it can receive a large list of poly_ids in one request
-const getPolygonsRoute: ServerRoute = {
-  method: "POST",
-  path: "/polygons",
-  handler: getPolygons,
+  handler: getPolygonsInBox,
   options: {
     auth: false,
   },
@@ -92,6 +102,19 @@ const searchRoute: ServerRoute = {
   method: "GET",
   path: "/search",
   handler: search,
+  options: {
+    auth: false,
+  },
+};
+
+/**
+ * Only used in development of analyse script
+ * Use POST so that it can receive a large list of poly_ids in one request.
+ */
+const getPolygonsRoute: ServerRoute = {
+  method: "POST",
+  path: "/polygonsDevSearch",
+  handler: getPolygons,
   options: {
     auth: false,
   },
