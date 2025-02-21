82feeab1ef8350ccf5732e8bc32cade00f3d0e0e
rogup <rohit5.gup@gmail.com>
2025-02-21 11:24:28 +0000

    Convert to EPSG:4326 projection in the first ogr2ogr command

--- scripts/gml-to-pending-inspire-polygons.sh
+++ scripts/gml-to-pending-inspire-polygons.sh
@@ -24,8 +24,9 @@ sed -i -e '0,/ xsi:schemaLocation="[^"]*"/ s///' "$1"
 # Delete temp sqlite file if it exists
 rm -f temp.sqlite
 
-# Convert the GML file to a SQLite database so we can manipulate the data
-ogr2ogr -f SQLite -skipfailures -nln polygons temp.sqlite $1
+# Transform the GML data into the EPSG:4326 projection and write to a SQLite database so we can
+# manipulate the data
+ogr2ogr -f SQLite -skipfailures -nln polygons -t_srs EPSG:4326 temp.sqlite $1
 
 # Add a council column
 ogrinfo temp.sqlite -sql "ALTER TABLE polygons ADD COLUMN council varchar(255)"
@@ -37,7 +38,7 @@ mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "DELETE FROM pending_inspire_polygo
 # Import the data into the pending_inspire_polygons table
 # Use GROUP BY to remove duplicate features in dataset with the same poly_id [removed, may need to re-add if an issue]
 # TODO: add -xyRes "0.0000001 deg" to round to 6 d.p., but this is only supported in GDAL 3.9+
-ogr2ogr -f MySQL -append -skipfailures -nln pending_inspire_polygons -t_srs EPSG:4326 \
+ogr2ogr -f MySQL -append -skipfailures -nln pending_inspire_polygons \
   "MySQL:$DB_NAME,user=$DB_USER,password=$DB_PASSWORD" temp.sqlite -unsetFid \
   -sql 'SELECT INSPIREID AS poly_id, geometry AS geom, council FROM polygons'
 
