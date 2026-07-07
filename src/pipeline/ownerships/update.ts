8177bf6515446261c080143d7d40d2ba64a338e6
Laura Thackray <laurasoftware21@gmail.com>
2026-07-07 16:02:50 +0100

    Add lots of types

--- src/pipeline/ownerships/update.ts
+++ src/pipeline/ownerships/update.ts
@@ -1,19 +1,22 @@
-import axios from "axios";
 import chunk from "lodash.chunk";
 import {
   deleteAllLandOwnerships,
   bulkDeleteLandOwnerships,
   bulkCreateOrUpdateLandOwnerships,
+  RawOwnership,
 } from "../../queries/land-ownership-query.js";
 import {
   getLatestOwnershipDataDate,
   setPipelineLatestOwnershipData,
 } from "../../queries/query.js";
 import {
+  fileDownload,
   getDatasetHistory,
+  getFullOverseasDataset,
+  getFullUKDataset,
   getLatestDatasets,
-  pipeZippedCsvFromUrlIntoFun,
-} from "./helpers.js";
+} from "./gov-api-client.js";
+import { pipeZippedCsvFromUrlIntoFun } from "./csv.js";
 import { logger } from "../logger.js";
 import { notifyMatrix } from "../util.js";
 
@@ -36,18 +39,29 @@ export const updateOwnerships = async (options: any) => {
     // datasets from Nov 2017 (the first set of data provided by the gov API in the current data
     // format)
     logger.info(
-      "Download the first full set of ownership data published in Nov 2017"
+      "Download the first full set of ownership data published in Nov 2017",
     );
     await downloadOwnershipsFullData(11, 2017);
     latestOwnershipDataDate = new Date("2017-11-28");
   }
 
   const ccodHistoricalDatasets = await getDatasetHistory(false);
+  if (!ccodHistoricalDatasets) {
+    return;
+  }
   const ocodHistoricalDatasets = await getDatasetHistory(true);
-
+  if (!ocodHistoricalDatasets) {
+    return;
+  }
   // Add the latest datasets which are (annoyingly) not included in the history API's response
   const latestCcodDatasets = await getLatestDatasets(false);
+  if (!latestCcodDatasets) {
+    return;
+  }
   const latestOcodDatasets = await getLatestDatasets(true);
+  if (!latestOcodDatasets) {
+    return;
+  }
 
   const unsortedListOfDatasets = [
     ...ccodHistoricalDatasets,
@@ -60,25 +74,25 @@ export const updateOwnerships = async (options: any) => {
     .filter(
       (dataset) =>
         // only keep monthly 'change only updates'
-        dataset.filename.includes("_COU_") &&
+        dataset.fileName.includes("_COU_") &&
         // only keep new files since the latest pipeline run
-        new Date(dataset.unsorted_date) > latestOwnershipDataDate
+        new Date(dataset.unsortedDate) > latestOwnershipDataDate,
     )
     // Sort chronologically (oldest first)
-    .sort((a, b) => (a.unsorted_date > b.unsorted_date ? 1 : -1));
+    .sort((a, b) => (a.unsortedDate > b.unsortedDate ? 1 : -1));
 
   logger.info(`There are ${filesToProcess.length} change files to process`);
 
   for (const [index, file] of filesToProcess.entries()) {
     logger.info(
-      `Processing ownership change file ${file.filename}, size ${file.file_size}`
+      `Processing ownership change file ${file.fileName}, size ${file.fileSize}`,
     );
 
-    const ownershipAdditions = [];
-    const ownershipDeletions = [];
+    const ownershipAdditions: RawOwnership[] = [];
+    const ownershipDeletions: RawOwnership[] = [];
 
     /** Thhe function we'll use to process each CSV row and add it to the apprioriate array */
-    const addOwnershipToArray = async (ownership: any) => {
+    const addOwnershipToArray = async (ownership: RawOwnership) => {
       if (!ownership || ownership["Title Number"] === "Row Count:") {
         // This is the last row of the CSV, which we can ignore
         return;
@@ -93,20 +107,16 @@ export const updateOwnerships = async (options: any) => {
           break;
         default:
           logger.error(
-            { filename: file.filename, ownership },
-            "No change indicator... we don't expect this!"
+            { filename: file.fileName, ownership },
+            "No change indicator... we don't expect this!",
           );
           throw new Error(
-            `No change indicator for ownership title ${ownership["Title Number"]} in file ${file.filename}`
+            `No change indicator for ownership title ${ownership["Title Number"]} in file ${file.fileName}`,
           );
       }
     };
 
-    const fileResponse = await axios.get(file.download, {
-      headers: {
-        Authorization: process.env.GOV_API_KEY,
-      },
-    });
+    const fileResponse = await fileDownload(file);
 
     // The change files are small enough to keep all the data in memory, so we can just use a
     // chunk size of 1 and then do the DB operations later. We want to do this so that we can
@@ -115,12 +125,12 @@ export const updateOwnerships = async (options: any) => {
       fileResponse.data.result.download_url,
       (ownershipsChunk) => addOwnershipToArray(ownershipsChunk[0]),
       1,
-      false
+      false,
     );
 
     // First processs deletions, then additions (so we don't delete new data)
     const ownershipDeletionTitleNos = ownershipDeletions.map(
-      (ownership) => ownership["Title Number"]
+      (ownership) => ownership["Title Number"],
     );
     logger.info(`Deleting ${ownershipDeletionTitleNos.length} ownerships`);
     await bulkDeleteLandOwnerships(ownershipDeletionTitleNos);
@@ -139,14 +149,14 @@ export const updateOwnerships = async (options: any) => {
       await bulkCreateOrUpdateLandOwnerships(chunk, file.type === "ocod");
     }
 
-    logger.info(`Finished processing ${file.filename}`);
+    logger.info(`Finished processing ${file.fileName}`);
 
     // If there are no more files from the same date to process, update DB with this latest date
     if (
       !filesToProcess[index + 1] ||
-      filesToProcess[index + 1].unsorted_date > file.unsorted_date
+      filesToProcess[index + 1].unsortedDate > file.unsortedDate
     ) {
-      await setPipelineLatestOwnershipData(file.unsorted_date);
+      await setPipelineLatestOwnershipData(file.unsortedDate);
     }
   }
 
@@ -173,57 +183,37 @@ async function downloadOwnershipsFullData(month: number, year: number) {
     await bulkCreateOrUpdateLandOwnerships(ownerships, overseas, false);
   };
 
-  const datasetUKResponse = await axios.get(
-    `${process.env.GOV_API_URL}/datasets/history/ccod/CCOD_FULL_${year}_${paddedMonth}.zip`,
-    {
-      headers: {
-        Authorization: process.env.GOV_API_KEY,
-      },
-    }
-  );
-
-  if (datasetUKResponse.status !== 200) {
-    logger.error(
-      `We failed to get UK data for ${paddedMonth}/${year} , status code: ${datasetUKResponse.status}`
-    );
+  const datasetUKResponse = await getFullUKDataset(month, year);
+  if (!datasetUKResponse) {
     return;
   }
 
   // Reset the table to avoid conflicting data
   await deleteAllLandOwnerships();
+  // Reset the history table to avoid conflicting data
+  // await deleteAllHistoricLandOwnerships();
 
   await pipeZippedCsvFromUrlIntoFun(
-    datasetUKResponse.data.result.download_url,
+    datasetUKResponse.downloadUrl,
     (ownership) => processOwnership(ownership, false),
-    20000
+    20000,
   );
 
-  const datasetOverseasResponse = await axios.get(
-    `${process.env.GOV_API_URL}/datasets/history/ocod/OCOD_FULL_${year}_${paddedMonth}.zip`,
-    {
-      headers: {
-        Authorization: process.env.GOV_API_KEY,
-      },
-    }
-  );
-
-  if (datasetOverseasResponse.status !== 200) {
-    logger.error(
-      `We failed to get overseas data for ${paddedMonth}/${year} , status code: ${datasetOverseasResponse.status}`
-    );
+  const datasetOverseasResponse = await getFullOverseasDataset(month, year);
+  if (!datasetOverseasResponse) {
     return;
   }
 
   await pipeZippedCsvFromUrlIntoFun(
-    datasetOverseasResponse.data.result.download_url,
+    datasetOverseasResponse.downloadUrl,
     (ownership) => processOwnership(ownership, true),
-    20000
+    20000,
   );
 
   logger.info(
-    `Finished downloading the whole UK and overseas companies data from ${paddedMonth}/${year}`
+    `Finished downloading the whole UK and overseas companies data from ${paddedMonth}/${year}`,
   );
   await setPipelineLatestOwnershipData(
-    `${year}-${paddedMonth}-28` // data is valid until the start of next month, so the exact day doesn't really matter
+    `${year}-${paddedMonth}-28`, // data is valid until the start of next month, so the exact day doesn't really matter
   );
 }
