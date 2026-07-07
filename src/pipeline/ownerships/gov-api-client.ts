14b7a0d46833ed66d78c74d3f555f4b72f8098bd
Laura Thackray <laurasoftware21@gmail.com>
2026-07-07 17:27:13 +0100

    fix more types and fix status code checks - axios throws

--- src/pipeline/ownerships/gov-api-client.ts
+++ src/pipeline/ownerships/gov-api-client.ts
@@ -5,7 +5,7 @@ type DatasetHistoryResponse = {
   dataset: "ocod" | "ccod";
   dataset_history: {
     file_size: string;
-    file_name: string;
+    filename: string;
     last_updated: string;
     unsorted_date: string;
   }[];
@@ -44,9 +44,23 @@ type LatestDataset = {
   unsortedDate: string;
 };
 
-type FullDataSet = {
+type DataSet = {
   downloadUrl: string;
 };
+
+type FileDownloadResponse =
+  | {
+      success: true;
+      result: {
+        download_url: string;
+        resource: string;
+        valid_for_seconds: number;
+      };
+    }
+  | {
+      success: false;
+      error: string;
+    };
 
 export const govApiClient = axios.create({
   baseURL: process.env.GOV_API_URL,
@@ -63,77 +77,71 @@ export const getDatasetHistory = async (
   overseas: boolean,
 ): Promise<DatasetHistory[] | null> => {
   const type = overseas ? "ocod" : "ccod";
-  const response = await govApiClient.get<DatasetHistoryResponse>(
-    `/datasets/history/${type}`,
-  );
-  if (response.status !== 200) {
-    logger.error(
-      `We failed to get ${type} dataset history, status code: ${response.status}`,
+  try {
+    const response = await govApiClient.get<DatasetHistoryResponse>(
+      `/datasets/history/${type}`,
     );
+    return response.data.dataset_history.map((dataset) => ({
+      type,
+      lastUpdated: dataset.last_updated,
+      fileSize: dataset.file_size,
+      fileName: dataset.filename,
+      // convert to YYYY-MM-DD format
+      unsortedDate: dataset.unsorted_date.split("-").reverse().join("-"),
+      download: `${process.env.GOV_API_URL}/datasets/history/${type}/${dataset.filename}`,
+    }));
+  } catch (err) {
+    logger.error(err, `Failed to get ${type} dataset history`);
     return null;
   }
-  return response.data.dataset_history.map((dataset) => ({
-    ...dataset,
-    type,
-    lastUpdated: dataset.last_updated,
-    fileSize: dataset.file_size,
-    fileName: dataset.file_name,
-    // convert to YYYY-MM-DD format
-    unsortedDate: dataset.unsorted_date.split("-").reverse().join("-"),
-    download: `${process.env.GOV_API_URL}/datasets/history/${type}/${dataset.file_name}`,
-  }));
 };
 
 /**
  * Retrieves the latest datasets based on their type.
- * @param overseas get latest datasets for overseas data *
+ * @param overseas get latest datasets for overseas data
  */
 export const getLatestDatasets = async (
   overseas: boolean,
 ): Promise<LatestDataset[] | null> => {
   const type = overseas ? "ocod" : "ccod";
-  const response = await govApiClient.get<LatestDatasetResponse>(
-    `/datasets/${type}`,
-  );
-  if (response.status !== 200) {
-    logger.error(
-      `We failed to get the latest ${type} dataset, status code: ${response.status}`,
+  try {
+    const response = await govApiClient.get<LatestDatasetResponse>(
+      `/datasets/${type}`,
     );
+    return response.data.result.resources.map((dataset) => ({
+      fileName: dataset.file_name,
+      type,
+      fileSize: dataset.file_size,
+      download: `${process.env.GOV_API_URL}/datasets/${type}/${dataset.file_name}`,
+      unsortedDate: new Date(response.data.result.last_updated)
+        .toISOString()
+        .split("T")[0],
+    }));
+  } catch (err) {
+    logger.error(err, `Failed to get latest ${type} datasets`);
     return null;
   }
-
-  return response.data.result.resources.map((dataset) => ({
-    fileName: dataset.file_name,
-    type,
-    fileSize: dataset.file_size,
-    download: `${process.env.GOV_API_URL}/datasets/${type}/${dataset.file_name}`,
-    unsortedDate: new Date(response.data.result.last_updated)
-      .toISOString()
-      .split("T")[0],
-  }));
 };
 
 /**
  * Retrieves the full UK dataset for a specific month and year.
  * @param month The month for which to retrieve data.
  * @param year The year for which to retrieve data.
-
  */
 export const getFullUKDataset = async (
   month: number,
   year: number,
-): Promise<FullDataSet | null> => {
+): Promise<DataSet | null> => {
   const paddedMonth = String(month).padStart(2, "0");
-  const response = await govApiClient.get<FullDatasetResponse>(
-    `/datasets/history/ccod/CCOD_FULL_${year}_${paddedMonth}.zip`,
-  );
-  if (response.status !== 200) {
-    logger.error(
-      `We failed to get UK data for ${paddedMonth}/${year}, status code: ${response.status}`,
+  try {
+    const response = await govApiClient.get<FullDatasetResponse>(
+      `/datasets/history/ccod/CCOD_FULL_${year}_${paddedMonth}.zip`,
     );
+    return { downloadUrl: response.data.result.download_url };
+  } catch (err) {
+    logger.error(err, `Failed to get UK dataset for ${paddedMonth}/${year}`);
     return null;
   }
-  return { downloadUrl: response.data.result.download_url };
 };
 
 /**
@@ -144,20 +152,40 @@ export const getFullUKDataset = async (
 export const getFullOverseasDataset = async (
   month: number,
   year: number,
-): Promise<FullDataSet | null> => {
+): Promise<DataSet | null> => {
   const paddedMonth = String(month).padStart(2, "0");
-  const response = await govApiClient.get<FullDatasetResponse>(
-    `/datasets/history/ocod/OCOD_FULL_${year}_${paddedMonth}.zip`,
-  );
-  if (response.status !== 200) {
+  try {
+    const response = await govApiClient.get<FullDatasetResponse>(
+      `/datasets/history/ocod/OCOD_FULL_${year}_${paddedMonth}.zip`,
+    );
+    return { downloadUrl: response.data.result.download_url };
+  } catch (err) {
     logger.error(
-      `We failed to get overseas data for ${paddedMonth}/${year}, status code: ${response.status}`,
+      err,
+      `Failed to get overseas dataset for ${paddedMonth}/${year}`,
     );
     return null;
   }
-  return { downloadUrl: response.data.result.download_url };
 };
 
-export const fileDownload = async (file: any) => {
-  return await govApiClient.get(file.download);
+/**
+ * Downloads a file from the gov API.
+ * @param downloadUrl The URL of the file to download.
+ */
+export const datasetDownload = async (
+  downloadUrl: string,
+): Promise<DataSet | null> => {
+  try {
+    const response = await govApiClient.get<FileDownloadResponse>(downloadUrl);
+    if (response.data.success !== true) {
+      logger.error(
+        `Failed to download file from ${downloadUrl}: ${response.data.error}`,
+      );
+      return null;
+    }
+    return { downloadUrl: response.data.result.download_url };
+  } catch (err) {
+    logger.error(err, `Failed to download file from ${downloadUrl}`);
+    return null;
+  }
 };
